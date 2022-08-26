c106869efbd570ff1e4213ec66589943322d7c4c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-08-26 22:46:38 +0530

    Added user.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const user = require('./routes/user')
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use('/user',user)
 
 app.listen(port,(req,res) => {
     console.log(`Example app listening on port ${port}`);
--- configs/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    _database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'Milasha99@',
-        database: 'facebook_app'
-    }
-}
