3f52425b731a76b2d80ed734ac795f8595a14a8e
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-10 23:33:45 +0530

    Added user.models.js

---
+++ Models/user.models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+    surName:{
+        type: String,
+        required: true
+    },
+    Gender:{
+        type: String,
+        required:true
+    },
+    dateOfBirth:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    }
+
+})
+
+module.exports = mongoose.model('User', userSchema)
