01cef68acaee03ea4e52c565ffbc7f175b53c9f1
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-10 12:26:53 +0530

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
 const user = require('./routes/user')
 
 const app = express();
 const port = 4000;
+
+const url = 'mongodb://127.0.0.1/express'
+
+mongoose.connect(url,{useNewUrlParser: true})
+const con = mongoose.connection
+
+con.on("open", ()=>{
+    console.log('MongoDB Connected');
+})
 
 app.use(express.json());
 app.use('/user',user)
