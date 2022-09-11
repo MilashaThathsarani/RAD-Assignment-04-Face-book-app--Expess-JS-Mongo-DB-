a92f5b2d828beb2417af34df37e22bcf4c95fc17
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-11 10:28:52 +0530

    Post add done

--- routes/post.js
+++ routes/post.js
@@ -2,4 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../Models/post.models')
 
+
+router.post('/',async (req,res) =>{
+    const post = new Post({
+        userId: req.body.userId,
+        date : req.body.date,
+        time: req.body.time,
+        title: req.body.title,
+        body: req.body.body
+    })
+
+    try {
+        const response = await post.save()
+        res.json(response)
+    }catch (err) {
+        res.send('Err:' + err)
+    }
+})
+
+
 module.exports = router
